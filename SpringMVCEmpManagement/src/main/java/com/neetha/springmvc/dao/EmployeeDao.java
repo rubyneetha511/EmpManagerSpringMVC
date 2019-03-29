dcb4cae0e5acf3b9276204e217c4b7ce4dddb73c
Neetha Prabhu <rubyneetha511@gmail.com>
2019-03-28 23:04:48 -0700

    Initial commit

---
+++ SpringMVCEmpManagement/src/main/java/com/neetha/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.neetha.springmvc.dao;
+
+import java.util.List;
+
+import com.neetha.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
